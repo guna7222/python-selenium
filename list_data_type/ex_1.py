b8ddf9d16e195ee905399d7fdea50a5d94ea4dfe
guna7222 <rrgunasekhar@gmail.com>
2025-04-08 05:27:43 +0100

    list data type

---
+++ list_data_type/ex_1.py
@@ -0,0 +1,9 @@
+my_list =  ['test1', 5, 5.7, 'test2']
+print(my_list)
+
+# list items are ordered, allow duplicates, changeable and index starts from 0
+
+print(my_list[0]) # test1
+
+# length of the list
+print(len(my_list))
---
+++ list_data_type/list_constructor.py
@@ -0,0 +1,4 @@
+new_list = list(('Github', 'go language'))
+print(new_list)
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
